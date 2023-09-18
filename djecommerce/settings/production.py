7a68952e185e7d3930a69cc2b9e732634ad12396
codingembryo <obam2780@gmail.com>
2023-09-18 11:57:24 +0100

    E commerce from Justdjango

---
+++ djecommerce/settings/production.py
@@ -0,0 +1,38 @@
+from .base import *
+
+DEBUG = config('DEBUG', cast=bool)
+ALLOWED_HOSTS = ['ip-address', 'www.your-website.com']
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': ''
+    }
+}
+
+STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
+
+
+# base.py
+
+# Common settings for all environments
+# Use a real email backend for production
+# development.py
+
+
+# Development-specific settings
+# Use console email backend for development
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_EMAIL_VERIFICATION = "none"  # No email verification in development
